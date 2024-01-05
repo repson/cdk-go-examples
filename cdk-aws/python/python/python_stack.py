7287727fc3eceea655e1430f5bd897133997bcf9
Isaac Peña <info@ipenat.com>
2024-01-05 23:06:13 +0100

    README updated

---
+++ cdk-aws/python/python/python_stack.py
@@ -0,0 +1,24 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class PythonStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self, "MyFirstBucket",
+            versioned=True,
+        )
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "PythonQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
